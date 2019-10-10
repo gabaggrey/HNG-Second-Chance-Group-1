b43aac07dd7ef86ecf73ef135d3730a309c9d802
Niscolinx <munisco12@gmail.com>
2019-10-10 19:12:25 +0100

    added frontend authentication to the login page

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+const inpText = document.getElementById('inp-text')
+const inpPass = document.getElementById('inp-pass')
+const button = document.querySelector('.button')
+const message = document.querySelector('.message')
+
+button.addEventListener('click', e => {
+    e.preventDefault()
+
+    if (inpPass.value !== '' || inpText.value !== '') {
+        if (inpPass.value.length < 6) {
+            message.style.display = 'block'
+            message.innerHTML = 'password must be 6 or more'
+        }
+        else if (inpText.value.length < 5) {
+            message.style.display = 'block'
+            message.innerHTML = 'Username must be 5 or more'
+        }
+        else {
+            message.style.display = 'none'
+        }
+    } else {
+        message.style.display = 'block'
+    }
+})
